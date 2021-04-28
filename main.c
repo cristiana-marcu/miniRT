085f0f5a57e061fe9723c928ec2d63cb8f77fc5d
RAMON <RAMON@MBP-de-RAMON.home>
2021-04-28 11:15:32 +0200

    Projectile printing

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cmarcu <cmarcu@student.42.fr>              +#+  +:+       +#+        */
+/*   By: RAMON <RAMON@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/21 15:39:49 by cmarcu            #+#    #+#             */
-/*   Updated: 2021/04/23 17:01:20 by cmarcu           ###   ########.fr       */
+/*   Updated: 2021/04/28 11:14:45 by RAMON            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,49 +30,46 @@ typedef struct s_environment
 t_projectile tick(t_environment env, t_projectile proj)
 {
 	t_projectile res;
-
+	
 	vec3_add(res.position, proj.position, proj.velocity);
 	vec3_add(res.velocity, proj.velocity, env.gravity);
 	vec3_add(res.velocity, res.velocity, env.wind);
 	return (res);
 }
 
-// int main()
-// {
-// 	t_projectile proj;
-// 	proj.position[0] = 0;
-// 	proj.position[1] = 1;
-// 	proj.position[2] = 0;
-// 	proj.velocity[0] = 1;
-// 	proj.velocity[1] = 1;
-// 	proj.velocity[2] = 0;
-
-// 	t_environment env;
-// 	env.gravity[0] = 0;
-// 	env.gravity[1] = -0.1;
-// 	env.gravity[2] = 0;
-// 	env.wind[0] = -0.1;
-// 	env.wind[1] = 0;
-// 	env.wind[2] = 0;
-// 	while (proj.position[1] > 0)
-// 	{
-// 		proj = tick(env, proj);
-// 		printf("%f, %f\n", proj.position[0], proj.position[1]);
-// 	}
-// }
-
 int	main(void)
 {
 	void	*mlx;
 	void	*mlx_win;
 	t_data	img;
+	t_projectile proj;
+	proj.position[0] = 0;
+	proj.position[1] = 1;
+	proj.position[2] = 0;
+	proj.velocity[0] = 1;
+	proj.velocity[1] = 1.8;
+	proj.velocity[2] = 0;
+
+	t_environment env;
+	env.gravity[0] = 0;
+	env.gravity[1] = -0.1;
+	env.gravity[2] = 0;
+	env.wind[0] = -0.1;
+	env.wind[1] = 0;
+	env.wind[2] = 0;
 
 	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1920, 1080, "miniRT");
-	img.img = mlx_new_image(mlx, 1920, 1080);
+	mlx_win = mlx_new_window(mlx, 700, 700, "miniRT");
+	img.img = mlx_new_image(mlx, 700, 700);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
 									&img.endian);
-	my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
+	vec3_normalize(proj.velocity, proj.velocity);
+	vec3_mult(proj.velocity, proj.velocity, 11.25);
+	while (proj.position[1] >= 0 && proj.position[0] >= 0)
+	{
+		proj = tick(env, proj);
+		my_mlx_pixel_put(&img, 700 - (int)proj.position[0], 700 - (int)proj.position[1], 0x00FF0000);
+	}
 	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
 	mlx_loop(mlx);
 }
