287fd08f920071c52dd95134aec0d8ff3974086a
Cristiana nicoleta Marcu <cmarcu@c1r7s2.42madrid.com>
2023-03-15 18:54:38 +0100

    Esc key and window X hook implemented

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: cmarcu <cmarcu@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/21 15:39:49 by cmarcu            #+#    #+#             */
-/*   Updated: 2023/03/14 19:10:49 by cmarcu           ###   ########.fr       */
+/*   Updated: 2023/03/15 18:53:31 by cmarcu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,9 +31,9 @@ int	close()
 
 int	main(void)
 {
-	void	*mlx;
-	void	*mlx_win;
-	t_data	img; //TODO
+	//void	*mlx;
+	//void	*mlx_win;
+	t_data	data; //TODO
 
 	const double aspect_ratio = 16.0 / 9.0;
     const int image_width = 720;
@@ -48,22 +48,22 @@ int	main(void)
     t_vec3 vertical = vctor(0, viewport_height, 0);
     t_vec3 lower_left_corner = vec3_subs(vec3_subs(origin, vec3_division(horizontal, 2)), vec3_subs(vec3_division(vertical, 2), vctor(0, 0, focal_length)));
 
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, image_width, image_height, "miniRT");
-	img.img = mlx_new_image(mlx, image_width, image_height);
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
-									&img.endian);
+	data.mlx = mlx_init();
+	data.win = mlx_new_window(data.mlx, image_width, image_height, "miniRT");
+	data.img = mlx_new_image(data.mlx, image_width, image_height);
+	data.addr = mlx_get_data_addr(data.img, &data.bits_per_pixel, &data.line_length,
+									&data.endian);
 	for (int j = image_height-1; j >= 0; --j) {
          for (int i = 0; i < image_width; ++i) {
             double u = (double)i / (image_width-1);
             double v = (double)j / (image_height-1);
             t_ray r = rctor(origin, vec3_add(vec3_add(lower_left_corner, vec3_mult(horizontal, u)), vec3_subs(vec3_mult(vertical, v), origin)));
             t_vec3 pixel_color = ray_color(&r);
-			my_mlx_pixel_put(&img, i, j, vec3_toInt(pixel_color)*1000);
+			my_mlx_pixel_put(&data, i, j, vec3_toInt(pixel_color)*1000);
         }
     }
-	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
-	mlx_key_hook(mlx_win, hook_keydown, &img);
-	mlx_hook(mlx_win, 17, 0, close, NULL);
-	mlx_loop(mlx);
+	mlx_put_image_to_window(data.mlx,data.win, data.img, 0, 0);
+	mlx_key_hook(data.win, hook_keydown, &data.mlx);
+	mlx_hook(data.win, 17, 0, close, NULL);
+	mlx_loop(data.mlx);
 }
