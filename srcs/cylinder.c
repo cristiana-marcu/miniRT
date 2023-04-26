c3eb72fc84b398309421211f78f61b5814b118b1
Cristiana nicoleta Marcu <cmarcu@c1r7s2.42madrid.com>
2023-04-26 21:15:51 +0200

    printing cylinder body, but not top and bottom intersections

--- srcs/cylinder.c
+++ srcs/cylinder.c
@@ -6,7 +6,7 @@
 /*   By: cmarcu <cmarcu@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/18 17:51:40 by cmarcu            #+#    #+#             */
-/*   Updated: 2023/04/26 20:50:47 by cmarcu           ###   ########.fr       */
+/*   Updated: 2023/04/26 21:14:53 by cmarcu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,24 +31,25 @@ t_cylinder *new_cylinder(t_vec3 pos, t_vec3 N, double r, double H, t_vec3 color)
 	return (cylinder);
 }
 
-bool hit_cylinder(t_object_list *obj, t_ray *ray,t_hit_record *rec)
+bool hit_cylinder(t_object_list *obj, t_ray *ray, t_hit_record *rec)
 {
-	t_cylinder *cyl;
+    t_cylinder *cyl = (t_cylinder*)obj->obj;
 
-	cyl = (t_cylinder*)obj->obj;
-	
-	t_vec3 p1p0 = vec3_subs(ray->origin, cyl->pos);
-    t_vec3 VxN = vec3_cross(ray->direction, cyl->N);
-    t_vec3 VxNxN = vec3_cross(VxN, cyl->N);
-    double a = vec3_dot(VxNxN, VxNxN) / vec3_dot(cyl->N, cyl->N);
-    double b = 2 * vec3_dot(VxNxN, vec3_cross(p1p0, cyl->N)) / vec3_dot(cyl->N, cyl->N);
-    double c = (vec3_dot(vec3_cross(p1p0, cyl->N), vec3_cross(p1p0, cyl->N)) - (cyl->r * cyl->r)) / vec3_dot(cyl->N, cyl->N);
-    double discriminant = b * b - 4 * a * c;
-    if (discriminant < 0)
+    t_vec3 co = vec3_subs(ray->origin, cyl->pos);
+    double a = vec3_dot(ray->direction, ray->direction) - pow(vec3_dot(ray->direction, cyl->N), 2);
+    double b = 2 * (vec3_dot(ray->direction, co) - vec3_dot(ray->direction, cyl->N) * vec3_dot(co, cyl->N));
+    double c = vec3_dot(co, co) - pow(vec3_dot(co, cyl->N), 2) - pow(cyl->r, 2);
+
+    double delta = b * b - 4 * a * c;
+
+    if (delta < 0)
         return (false);
-    double t1 = (-b + sqrt(discriminant)) / (2 * a);
-    double t2 = (-b - sqrt(discriminant)) / (2 * a);
+
+    double t1 = (-b + sqrt(delta)) / (2 * a);
+    double t2 = (-b - sqrt(delta)) / (2 * a);
+
     double t_closest, t_farthest;
+
     if (t1 > t2)
     {
         t_closest = t2;
@@ -59,29 +60,24 @@ bool hit_cylinder(t_object_list *obj, t_ray *ray,t_hit_record *rec)
         t_closest = t1;
         t_farthest = t2;
     }
-    t_vec3 point = rayAt(ray, t_closest);
-    t_vec3 vec_from_base_to_point = vec3_subs(point, cyl->pos);
-    double dist_to_cyl_axis_sq = vec3_dot(vec_from_base_to_point, vec_from_base_to_point) - pow(vec3_dot(vec_from_base_to_point, cyl->N), 2);
-    if (dist_to_cyl_axis_sq > cyl->r * cyl->r || t_closest < rec->t_min || t_closest > rec->t_max)
+
+    if (t_closest < rec->t_min || t_closest > rec->t_max)
     {
-        point = rayAt(ray, t_farthest);
-        vec_from_base_to_point = vec3_subs(point, cyl->pos); // Añade esta línea
-        dist_to_cyl_axis_sq = vec3_dot(vec_from_base_to_point, vec_from_base_to_point) - pow(vec3_dot(vec_from_base_to_point, cyl->N), 2);
-        if (dist_to_cyl_axis_sq > cyl->r * cyl->r || t_farthest < rec->t_min || t_farthest > rec->t_max)
+        if (t_farthest < rec->t_min || t_farthest > rec->t_max)
             return (false);
-        rec->t = t_farthest;
+        t_closest = t_farthest;
     }
-    else
-    {
-        rec->t = t_closest;
-    }
-    point = rayAt(ray, rec->t);
-    double height = vec3_dot(vec3_subs(point, cyl->pos), cyl->N);
+
+    t_vec3 hit_point = rayAt(ray, t_closest);
+    double height = vec3_dot(vec3_subs(hit_point, cyl->pos), cyl->N);
     if (height < 0 || height > cyl->H)
         return (false);
-    rec->hit_point = rayAt(ray, rec->t);
-    rec->N = vec3_normalize(vec3_subs(rec->hit_point, vec3_add(cyl->pos, vec3_mult(cyl->N, height))));
+    
+    rec->t = t_closest;
+    rec->hit_point = hit_point;
+    rec->N = vec3_normalize(vec3_subs(rec->hit_point, vec3_add(cyl->pos, vec3_mult(cyl->N, vec3_dot(vec3_subs(rec->hit_point, cyl->pos), cyl->N)))));
     set_face_normal(ray, rec);
     rec->t_max = rec->t;
+
     return (true);
 }
