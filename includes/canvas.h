3476d4cb988fa7cf0fb5e8c961c484f919bfcd97
Cristiana nicoleta Marcu <cmarcu@c1r9s6.42madrid.com>
2023-05-03 14:57:02 +0200

    some more norminette

--- includes/canvas.h
+++ includes/canvas.h
@@ -6,19 +6,19 @@
 /*   By: cmarcu <cmarcu@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/22 16:36:08 by cmarcu            #+#    #+#             */
-/*   Updated: 2023/04/15 17:55:05 by cmarcu           ###   ########.fr       */
+/*   Updated: 2023/05/03 14:24:47 by cmarcu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CANVAS_H
 # define CANVAS_H
 
-#include <math.h>
-#include <stdlib.h>
-#include <mlx.h>
-#include "vectors.h"
-#include "ray.h"
-#include "objects.h"
+# include <math.h>
+# include <stdlib.h>
+# include <mlx.h>
+# include "vectors.h"
+# include "ray.h"
+# include "objects.h"
 
 # define K_ESC 53
 
--- includes/objects.h
+++ includes/objects.h
@@ -6,132 +6,132 @@
 /*   By: cmarcu <cmarcu@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/18 15:02:03 by cmarcu            #+#    #+#             */
-/*   Updated: 2023/04/26 17:53:51 by cmarcu           ###   ########.fr       */
+/*   Updated: 2023/05/03 14:30:13 by cmarcu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef OBJECTS_H
 # define OBJECTS_H
 
-#include "vectors.h"
-#include "ray.h"
-#include <math.h>
-#include <stdlib.h>
-#include <mlx.h>
+# include "vectors.h"
+# include "ray.h"
+# include <math.h>
+# include <stdlib.h>
+# include <mlx.h>
 
-typedef struct s_object_list t_object_list;
+typedef struct s_object_list	t_object_list;
 
 typedef enum e_type
 {
 	SPHERE,
 	PLANE,
 	CYLINDER,
-    LIGHT
+	LIGHT
 }			t_type;
 
 typedef struct s_sphere
 {
-    t_vec3 center;
-    double r;
-    t_vec3 color;
-} t_sphere;
+	t_vec3	center;
+	double	r;
+	t_vec3	color;
+}	t_sphere;
 
 typedef struct s_plane
 {
-    t_vec3 pos;
-    t_vec3 N;
-    t_vec3 color;
-} t_plane;
+	t_vec3	pos;
+	t_vec3	N;
+	t_vec3	color;
+}	t_plane;
 
 typedef struct s_cylinder
 {
-    t_vec3 pos;
-    t_vec3 N;
-    double r;
-    double H;
-    t_vec3 color;
-} t_cylinder;
+	t_vec3	pos;
+	t_vec3	N;
+	double	r;
+	double	H;
+	t_vec3	color;
+}	t_cylinder;
 
 typedef struct s_ambientLight
 {
-    double range;
-    t_vec3 color;
-} t_ambientLight;
+	double	range;
+	t_vec3	color;
+}	t_ambientLight;
 
 typedef struct s_light
 {
-    t_vec3 pos;
-    double brightness;
-    t_vec3 color; //solo bonus
-} t_light;
+	t_vec3	pos;
+	double	brightness;
+	t_vec3	color; //solo bonus
+}	t_light;
 
 typedef struct s_camera
 {
-    t_vec3  from;
-    t_vec3  lookAt; //Normalized direction vector
-    double  HFOV;
-	double  viewport_height;
-	double  viewport_width;
-    t_vec3  w;
-    t_vec3  u;
-    t_vec3  v;
-    t_vec3  horizontal;
-    t_vec3  vertical;
-    t_vec3  lower_left_corner;
+	t_vec3	from;
+	t_vec3	lookAt; //Normalized direction vector
+	double	HFOV;
+	double	viewport_height;
+	double	viewport_width;
+	t_vec3	w;
+	t_vec3	u;
+	t_vec3	v;
+	t_vec3	horizontal;
+	t_vec3	vertical;
+	t_vec3	lower_left_corner;
 } t_camera;
 
 typedef struct s_hit_record
 {
-    t_vec3 hit_point;
-    t_vec3 N;
-    double t;
-    bool front_face;
-    double t_min;
-    double t_max;
-} t_hit_record;
+	t_vec3	hit_point;
+	t_vec3	N;
+	double	t;
+	bool	front_face;
+	double	t_min;
+	double	t_max;
+}	t_hit_record;
 
 typedef struct s_world
 {
-    t_object_list *objs;
-    t_object_list *lights;
-    t_ambientLight AL;
-    t_camera camera;
-    t_hit_record *rec;
-} t_world;
+	t_object_list	*objs;
+	t_object_list	*lights;
+	t_ambientLight	AL;
+	t_camera		camera;
+	t_hit_record	*rec;
+}	t_world;
 
 typedef struct s_ray
 {
-	t_vec3 origin;
-	t_vec3 direction;
-} t_ray;
+	t_vec3	origin;
+	t_vec3	direction;
+}	t_ray;
 
 typedef bool (*t_intersection_function)(t_object_list *, t_ray *, t_hit_record *);
 
 struct s_object_list
 {
-    t_type type;
-    void *obj;
-    void *next;
+	t_type	type;
+	void	*obj;
+	void	*next;
 };
 
 typedef struct s_canvas
 {
-	unsigned short width;
-	unsigned short height;
-	double aspect_ratio;
-} t_canvas;
+	unsigned short	width;
+	unsigned short	height;
+	double			aspect_ratio;
+}	t_canvas;
 
 typedef struct s_data {
 	void	*mlx;
 	void	*win;
 	void	*img;
 	char	*addr;
-	int	bits_per_pixel;
-	int	line_length;
-	int	endian;
-	t_canvas view;
-	t_world *world;
-} t_data;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+	t_canvas	view;
+	t_world	*world;
+}	t_data;
 
 bool hit_sphere(t_object_list *obj, t_ray *r,t_hit_record *rec);
 
