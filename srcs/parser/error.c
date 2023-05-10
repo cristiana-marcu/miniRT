906a6b164c6e2911b399d2718b2e5af99b2e8ea4
rontomano <rontomeromano@gmail.com>
2023-05-10 21:32:47 +0200

    exit with bitwise added

--- srcs/parser/error.c
+++ srcs/parser/error.c
@@ -15,9 +15,15 @@
 void print_err(uint16_t err)
 {
 	ft_putstr_fd("Error\n", STDERR_FILENO);
-	if (err & E_BADARG)
-		ft_putstr_fd("\t- Just one argument valid\n", 2);
-	else if (err & E_EXT)
+	if (err & E_BAD_ARG)
+		ft_putstr_fd("\t- Just one argument valid\n", STDERR_FILENO);
+	else if (err & E_EXTEN)
 		ft_putstr_fd("\t- File does not end with '.rt'\n", STDERR_FILENO);
-
+	else if (err & E_OPEN)
+		ft_putstr_fd("\t- Could not open the file\n", STDERR_FILENO);
+	if (err & E_EXIT)
+	{
+		ft_putstr_fd("Exit\n", STDERR_FILENO);
+		exit(err ^ E_EXIT);
+	}
 }
--- srcs/parser/parser.c
+++ srcs/parser/parser.c
@@ -1,18 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   parser.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: drontome <drontome@student.42.fr>          +#+  +:+       +#+        */
+/*   By: drontome <drontome@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/06 13:24:01 by drontome          #+#    #+#             */
-/*   Updated: 2023/05/08 18:32:06 by drontome         ###   ########.fr       */
+/*   Created: 2023/05/10 20:48:46 by drontome          #+#    #+#             */
+/*   Updated: 2023/05/10 20:48:55 by drontome         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minirt.h"
 
+# include "minirt.h"
 
+bool	check_scene(int argc, char **argv)
+{
+	int fd;
+
+	fd = 0;
+	if (argc != 2)
+		print_err(E_BAD_ARG | E_EXIT);
+	else if (ft_str_ends_with(argv[1], ".rt"))
+		print_err(E_EXTEN | E_EXIT);
+	else
+		fd = open(argv[1], O_RDONLY);
+	if (fd < 0)
+		print_err(E_OPEN | E_EXIT);
+	return (false);
+}
 
 int main(int argc, char **argv)
 {
