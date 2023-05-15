d9dd1d6b5601f9ca9914888e7af33e3525bbe464
Cristiana nicoleta Marcu <cmarcu@c1r3s2.42madrid.com>
2023-05-15 13:40:04 +0200

    adapted to last changes on world structure and pointers

--- srcs/lights.c
+++ srcs/lights.c
@@ -6,7 +6,7 @@
 /*   By: cmarcu <cmarcu@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/07 13:29:31 by cmarcu            #+#    #+#             */
-/*   Updated: 2023/05/10 07:28:03 by cmarcu           ###   ########.fr       */
+/*   Updated: 2023/05/15 13:38:46 by cmarcu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,7 +46,7 @@ t_vec3	ambient_light_on_obj(t_world *world)
 	t_vec3			amb_and_obj;
 	t_ambientLight	al;
 
-	al = *world->amb_light;
+	al = world->amb_light;
 	amb_and_obj = vctor(al.range * (al.color.x) * (world->rec->color.x),
 			al.range * (al.color.y) * (world->rec->color.y),
 			al.range * (al.color.z) * (world->rec->color.z));
@@ -61,8 +61,8 @@ t_vec3	diffuse_light_on_obj(t_world *world, t_ray shadow_ray)
 	dot_NL = vec3_dot(vec3_norm(world->rec->N), vec3_norm(shadow_ray.direction));
 	if (dot_NL < 0)
 		dot_NL = 0;
-	diffuse.x = world->light->brightness * world->rec->color.x * dot_NL;
-	diffuse.y = world->light->brightness * world->rec->color.y * dot_NL;
-	diffuse.z = world->light->brightness * world->rec->color.z * dot_NL;
+	diffuse.x = world->light.brightness * world->rec->color.x * dot_NL;
+	diffuse.y = world->light.brightness * world->rec->color.y * dot_NL;
+	diffuse.z = world->light.brightness * world->rec->color.z * dot_NL;
 	return (diffuse);
 }
--- srcs/ray.c
+++ srcs/ray.c
@@ -6,7 +6,7 @@
 /*   By: cmarcu <cmarcu@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/11 16:15:05 by cristianama       #+#    #+#             */
-/*   Updated: 2023/05/10 07:34:50 by cmarcu           ###   ########.fr       */
+/*   Updated: 2023/05/15 13:37:54 by cmarcu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,10 +55,10 @@ t_vec3	calculate_pixel_color(t_world *world)
 	t_object_list	*obj;
 	t_hit_record	shadow_rec;
 
-	shadow_ray = rctor(vec3_add(world->rec->hit_point, vec3_mult(world->rec->N, 0.001)), vec3_subs(world->light->pos, world->rec->hit_point));
+	shadow_ray = rctor(vec3_add(world->rec->hit_point, vec3_mult(world->rec->N, 0.001)), vec3_subs(world->light.pos, world->rec->hit_point));
 	obj = world->objs;
 	shadow_rec.t_min = EPSILON;
-	shadow_rec.t_max = vec3_magn(vec3_subs(world->light->pos, world->rec->hit_point));
+	shadow_rec.t_max = vec3_magn(vec3_subs(world->light.pos, world->rec->hit_point));
 	while (obj)
 	{
 		if (hit_object(obj, &shadow_ray, &shadow_rec))
